9668e726fe262faafdef730090ee237c2bed98fc
José Manuel Rodriguez Caballero <josephcmac@gmail.com>
2021-10-11 15:58:46 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,11 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening to ${PORT}`));
 app.disable('etag');//to guarantee that res.statusCode = 200, unless there is an error
 
+
+app.use("*",(req, res) =>{
+    res.send("<h1>Caballero|Account API</h1>");
+});
+
 app.use(accountRoutes);
 
 
